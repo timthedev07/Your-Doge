b36367321030f1b3361cd6a34641cc0abcdbea6f
Harvard90873 <65774333+Harvard90873@users.noreply.github.com>
2021-06-25 20:08:06 +0200

    still tweaking the orm config...

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -3,15 +3,19 @@ const MODE = process.env.NODE_ENV;
 const basedir = MODE === "production" ? "dist" : "src";
 const fileType = MODE === "production" ? "js" : "ts";
 
+const production = MODE === "production";
+
 module.exports = {
   type: "postgres",
-  host: "localhost",
+  host: production ? process.env.HP_HOST : "localhost",
   port: 5432,
-  username: process.env.POSTGRES_USERNAME,
-  password: process.env.POSTGRES_PASSWORD,
-  database: "homework-manager",
-  synchronize: true,
-  logging: false,
+  username: production
+    ? process.env.HP_USERNAME
+    : process.env.POSTGRES_USERNAME,
+  password: production
+    ? process.env.HP_PASSWORD
+    : process.env.POSTGRES_PASSWORD,
+  database: production ? process.env.HP_DATABASE : "homework-manager",
   entities: [`${basedir}/entity/**/*.${fileType}`],
   migrations: [`${basedir}/migrations/**/*.${fileType}`],
   subscribers: [`${basedir}/subscriber/**/*.${fileType}`],
@@ -19,5 +23,8 @@ module.exports = {
     entitiesDir: `${basedir}/entity`,
     migrationsDir: `${basedir}/migrations`,
     subscribersDir: `${basedir}/subscriber`,
+  },
+  extra: {
+    ssl: production,
   },
 };
