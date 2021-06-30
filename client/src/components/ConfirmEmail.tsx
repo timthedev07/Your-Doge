606a77009d1a22962bf14ca3147cd8ad5ea5714f
Harvard90873 <65774333+Harvard90873@users.noreply.github.com>
2021-06-30 21:03:54 +0200

    fixed account tab-switcher overflow

--- client/src/components/ConfirmEmail.tsx
+++ client/src/components/ConfirmEmail.tsx
@@ -1,14 +1,26 @@
 import React from "react";
+import { useConfirmEmailMutation } from "../generated/graphql";
 
 interface ConfirmEmailProps {
   token: string;
 }
 
 export const ConfirmEmail: React.FC<ConfirmEmailProps> = ({ token }) => {
+  const [confirmEmail] = useConfirmEmailMutation();
+
   return (
-    <div className="email-confirmation-card">
-      <h1>One step away.</h1>
-      <button>{token}</button>
+    <div className="email-confirmation">
+      <div className="email-confirmation-card">
+        <h1>One step away.</h1>
+        <button
+          onClick={async () => {
+            const res = await confirmEmail({ variables: { token } });
+            console.log(res);
+          }}
+        >
+          {token}
+        </button>
+      </div>
     </div>
   );
 };
