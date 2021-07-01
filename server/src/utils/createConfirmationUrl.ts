5ffa4ab489f4cea4db6c0a286eb24e394acf4336
Harvard90873 <65774333+Harvard90873@users.noreply.github.com>
2021-07-01 16:46:02 +0200

    shortened the life of a token

--- server/src/utils/createConfirmationUrl.ts
+++ server/src/utils/createConfirmationUrl.ts
@@ -4,7 +4,7 @@ import { redis } from "../redis";
 
 export const createConfirmationUrl = async (userId: number) => {
   const id = v4();
-  await redis.set(id, userId, "ex", 60 * 60 * 24); // expires in 1 day
+  await redis.set(id, userId, "ex", 60 * 60 * 8); // expires in 8 hours
   return `${
     process.env.NODE_ENV === "production" ? FRONTEND_URL : DEV_FRONTEND
   }/confirm/${id}`;
