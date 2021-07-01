9e7d0e0665d64ef1f1541c39709037c0251b31d1
Harvard90873 <65774333+Harvard90873@users.noreply.github.com>
2021-07-01 13:35:09 +0200

    Extended username maxlen and added other fields

--- server/src/entity/User.ts
+++ server/src/entity/User.ts
@@ -24,7 +24,7 @@ export class User extends BaseEntity {
   email: string;
 
   @Field(() => String)
-  @Column({ type: "varchar", length: "14" })
+  @Column({ type: "varchar", length: "14", unique: true })
   username: string;
 
   @Column("text")
