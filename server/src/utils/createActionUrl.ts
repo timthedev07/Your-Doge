b87ffa668ac65b55edf1dd9e78394824c0a9c701
Harvard90873 <65774333+Harvard90873@users.noreply.github.com>
2021-07-02 22:32:43 +0200

    enhanced logic
    added constant prefixes before tokens to clarify the purpose of each

--- server/src/utils/createActionUrl.ts
+++ server/src/utils/createActionUrl.ts
@@ -1,11 +1,15 @@
 import { v4 } from "uuid";
 import { DEV_FRONTEND, FRONTEND_URL } from "..";
+import { prefixMap } from "../constants/email";
 import { redis } from "../redis";
 
 export const createActionUrl = async (userId: number, action: string) => {
   const token = v4();
-  await redis.set(token, userId, "ex", 60 * 60 * 8); // expires in 8 hours
+
+  const prefixedToken = (prefixMap[action] || "dG9rZW4=:") + token;
+
+  await redis.set(prefixedToken, userId, "ex", 60 * 60 * 8); // expires in 8 hours
   return `${
     process.env.NODE_ENV === "production" ? FRONTEND_URL : DEV_FRONTEND
-  }/auth/${action}/${token}`;
+  }/auth/${action}/${prefixedToken}`;
 };
