b9330c6ea751035fd7040f98aef403e86034cc64
Harvard90873 <65774333+Harvard90873@users.noreply.github.com>
2021-07-02 22:15:22 +0200

    modification of the implementation
    adapated changes in routes in the front end and improved flexibility.

--- server/src/utils/createConfirmationUrl.ts
+++ server/src/utils/createConfirmationUrl.ts
@@ -2,10 +2,10 @@ import { v4 } from "uuid";
 import { DEV_FRONTEND, FRONTEND_URL } from "..";
 import { redis } from "../redis";
 
-export const createConfirmationUrl = async (userId: number) => {
-  const id = v4();
-  await redis.set(id, userId, "ex", 60 * 60 * 8); // expires in 8 hours
+export const createActionUrl = async (userId: number, action: string) => {
+  const token = v4();
+  await redis.set(token, userId, "ex", 60 * 60 * 8); // expires in 8 hours
   return `${
     process.env.NODE_ENV === "production" ? FRONTEND_URL : DEV_FRONTEND
-  }/confirm/${id}`;
+  }/auth/${action}/${token}`;
 };
