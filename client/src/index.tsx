09e04367ba51a217c129e787522c3a0e1c7dc94a
Harvard90873 <65774333+Harvard90873@users.noreply.github.com>
2021-07-04 21:00:14 +0200

    tweaked settings for testing purposes

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,18 +3,12 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { App } from "./App";
 import { generateApolloClient } from "./utils/clientGenerator";
 
+export const DEV_BACKEND_BASE_URL = "http://localhost:4000";
 export const USERS_BACKEND_BASE_URL =
-  // "https://homework-manager-users.herokuapp.com";
-  "http://localhost:4000";
-export const USERS_BACKEND_AUTH_BASE_URL = `${USERS_BACKEND_BASE_URL}/auth`;
+  "https://homework-manager-users.herokuapp.com";
+export const BACKEND_BASE_URL = "https://homework-manager-db0.herokuapp.com";
 
-// export const BACKEND_BASE_URL = "https://homework-manager-db0.herokuapp.com";
-// export const BACKEND_AUTH_BASE_URL = `${BACKEND_BASE_URL}/auth`;
-
-export const authClient = generateApolloClient(
-  USERS_BACKEND_BASE_URL,
-  USERS_BACKEND_AUTH_BASE_URL
-);
+export const authClient = generateApolloClient(USERS_BACKEND_BASE_URL);
 
 export const unknownErrMsg =
   "Sorry, an unknown error occurred, try again later, or contact our support team(bot.rem.autogenerate@gmail.com)";
