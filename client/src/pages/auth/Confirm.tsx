6246e5eabb6c73351626170892f93b503e12e48e
Harvard90873 <65774333+Harvard90873@users.noreply.github.com>
2021-07-04 21:15:07 +0200

    added inbox image

--- client/src/pages/auth/Confirm.tsx
+++ client/src/pages/auth/Confirm.tsx
@@ -3,6 +3,7 @@ import { Redirect, useParams } from "react-router";
 import { ConfirmEmail } from "../../components/confirmation/ConfirmEmail";
 import { ResendConfEmail } from "../../components/confirmation/ResendConfEmail";
 import { useMeQuery } from "../../generated/graphql";
+import { ReactComponent as Inbox } from "../../assets/images/inbox.svg";
 
 export interface ConfirmationRouteParams {
   token?: string;
@@ -29,6 +30,7 @@ export const Confirm: React.FC = () => {
         <div className="email-confirmation-card">
           <div>
             <h2 style={{ width: "100%" }}>Next Up, confirm your email...</h2>
+            <Inbox style={{ width: "200px" }} />
             <p>
               Check your inbox for further instructions, this step is mandatory
               for the purpose of your security and privacy. If you didn't
