0f62b50f1857449860ae9f6d512133b0a921e533
Harvard90873 <65774333+Harvard90873@users.noreply.github.com>
2021-07-06 20:24:16 +0200

    added middleware url

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,15 +7,19 @@ export const DEV_BACKEND_BASE_URL = "http://localhost:4000";
 export const USERS_BACKEND_BASE_URL =
   "https://homework-manager-users.herokuapp.com";
 export const BACKEND_BASE_URL = "https://homework-manager-db0.herokuapp.com";
+export const MIDDLEWARE_URL = "http://localhost:5000";
 
 export const authClient = generateApolloClient(DEV_BACKEND_BASE_URL);
+export const middlewareClient = generateApolloClient(MIDDLEWARE_URL);
 
 export const unknownErrMsg =
   "Sorry, an unknown error occurred, try again later, or contact our support team(bot.rem.autogenerate@gmail.com)";
 
 ReactDOM.render(
+  // <ApolloProvider client={middlewareClient}>
   <ApolloProvider client={authClient}>
     <App />
   </ApolloProvider>,
+  // </ApolloProvider>,
   document.getElementById("root")
 );
