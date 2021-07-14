f52146a0fdf89c9bed76fc6c766592170caed09e
Tim <harvard90873@gmail.com>
2021-07-14 14:29:23 +0200

    pseudo session implementation complete
    implemented a session like system with localstorage

---
+++ client/src/utils/localStorageExpiration.ts
@@ -0,0 +1,34 @@
+interface ExpiryObject {
+  value: any;
+  expiry: number;
+}
+/**
+ *
+ * @param key They key that maps to the value
+ * @param value The actual value
+ * @param validUntil A date
+ */
+export const setWithExpiry = (key: string, value: any, validUntil: number) => {
+  const item: ExpiryObject = {
+    value: value,
+    expiry: validUntil.valueOf(),
+  };
+
+  localStorage.setItem(key, JSON.stringify(item));
+};
+
+export const getWithExpiry = (key: string) => {
+  const itemStr = localStorage.getItem(key);
+  if (!itemStr) {
+    return null;
+  }
+
+  const item: ExpiryObject = JSON.parse(itemStr);
+  const now = new Date();
+
+  if (now.valueOf() > item.expiry) {
+    localStorage.removeItem(key);
+    return null;
+  }
+  return item.value;
+};
