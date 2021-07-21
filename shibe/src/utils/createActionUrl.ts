5fe78f738ae306739cb0598fe6e18942dbf022fe
Tim <harvard90873@gmail.com>
2021-07-21 18:05:29 +0200

    allow explicitly setting token valid duration

--- shibe/src/utils/createActionUrl.ts
+++ shibe/src/utils/createActionUrl.ts
@@ -3,11 +3,20 @@ import { FRONTEND } from "../index";
 import { prefixMap } from "../constants/email";
 import { redisClient } from "../redis";
 
-export const createActionUrl = async (userId: number, action: string) => {
+export const createActionUrl = async (
+  userId: number,
+  action: string,
+  durationInHours?: number
+) => {
   const token = v4();
 
   const prefixedToken = (prefixMap[action] || "dG9rZW4=:") + token;
 
-  redisClient.set(prefixedToken, `${userId}`, "ex", 60 * 60 * 8); // expires in 8 hours
+  redisClient.set(
+    prefixedToken,
+    `${userId}`,
+    "ex",
+    60 * 60 * (durationInHours || 8)
+  ); // expires in 8 hours by default(if a duration is not given)
   return `${FRONTEND}/auth/${action}/${prefixedToken}`;
 };
