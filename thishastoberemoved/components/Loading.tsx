4731a1422aebfcaa1b000a18237487ca4b5b5805
Tim <harvard90873@gmail.com>
2021-07-22 18:05:07 +0200

    migrated graphql codgen for homework related data

--- thishastoberemoved/components/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactComponent as SpinnerIcon } from "../assets/images/icons/spinner.svg";
-
-export const Loading = () => {
-  return (
-    <div id="loading">
-      <SpinnerIcon id="loading-spinner" />
-    </div>
-  );
-};
