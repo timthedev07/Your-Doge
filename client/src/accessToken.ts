69cf94804643fecc9665410a1f2dc7d10b46f43d
Tim <harvard90873@gmail.com>
2021-07-23 22:00:41 +0200

    move accesstoken.ts such that now it is included

--- client/src/accessToken.ts
+++ client/src/accessToken.ts
@@ -1,8 +1,12 @@
 export let accessToken = "";
 
+console.log(accessToken);
+
 export const setAccessToken = (s: string) => {
   accessToken = s;
+  console.log(accessToken);
 };
 export const getAccessToken = () => {
+  console.log(accessToken);
   return accessToken;
 };
