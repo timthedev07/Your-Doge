cad8c9b91a75dff589ec7117a3e78e85326c40a1
Tim <harvard90873@gmail.com>
2021-07-25 12:41:24 +0200

    expiration notice

--- shibe/src/constants/email.ts
+++ shibe/src/constants/email.ts
@@ -6,7 +6,7 @@ export const prefixMap: Record<string, string> = {
 export const CONFIRM_EMAIL_LETTER_CONTENT = `
 Dear User,
 
-At your request, Your Doge has sent you the following link so that you may verify your email and continue to use the service.
+At your request, Your Doge has sent you the following link so that you may verify your email and continue to use the service, and please note that the url would expire in 8 hours.
 If you received this email without registering on Your Doge, ignore it, and nothing will happen.
 
 Sincerely,
@@ -20,7 +20,7 @@ export const RESET_PASSWORD_LETTER_CONTENT = `
 Dear User,
 
 At your request, Your Doge has sent you the following link so that you may reset your password. If it was not at your request, then you should be aware that someone has entered your email address as theirs in the forgotten password section on the login page.
-
+Please note that the url would expire in 30 minutes.
 If you did not forget your password, ignore this email and no changes will be made to your account.
 
 Sincerely,
