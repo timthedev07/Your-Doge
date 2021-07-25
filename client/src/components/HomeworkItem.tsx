837efe8dfdc34e491c6a62c184fcf4db8c4e7597
Tim <harvard90873@gmail.com>
2021-07-25 23:08:23 +0200

    i don't know, imma leave it for now but...
    remove redundant interface declaration

--- client/src/components/HomeworkItem.tsx
+++ client/src/components/HomeworkItem.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 
-interface HomeworkItemProps {}
-
-export const HomeworkItem: React.FC<HomeworkItemProps> = ({}) => {
+export const HomeworkItem: React.FC = ({}) => {
   return (
     <div className="homework-item">
       <div></div>
