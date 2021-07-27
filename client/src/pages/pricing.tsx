35115c0bb15d4f2d91bdc0ca00754c43e48ef063
Tim <harvard90873@gmail.com>
2021-07-27 16:36:41 +0200

    doge dancing gifs

--- client/src/pages/pricing.tsx
+++ client/src/pages/pricing.tsx
@@ -4,15 +4,26 @@ interface PricingProps {}
 
 const Pricing: React.FC<PricingProps> = ({}) => {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <h1 style={{ textAlign: "center" }}>IT&apos;S FREE</h1>
-    </div>
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "60vh",
+        }}
+      >
+        <h1 style={{ textAlign: "center" }}>IT&apos;S FREE</h1>
+      </div>
+      <img
+        src="https://i.pinimg.com/originals/62/9b/06/629b065f07bd59226785164e6a2310f1.gif"
+        alt="doge dancing"
+      />
+      <img
+        src="https://media1.giphy.com/media/9C1nyePnovqlpEYFMD/giphy.gif"
+        alt="doge"
+      />
+    </>
   );
 };
 
