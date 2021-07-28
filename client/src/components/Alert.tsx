493e19287328cbddd7af36057d4ed45324026cca
Tim <harvard90873@gmail.com>
2021-07-28 17:55:53 +0200

    refactor: small parts of alert's styles

--- client/src/components/Alert.tsx
+++ client/src/components/Alert.tsx
@@ -1,21 +1,34 @@
 import React from "react";
 import { AlertProps } from "../types/props";
 
-export const Alert: React.FC<AlertProps> = (props) => {
+const TEXT_MAP = {
+  success: "Awesome, everything worked just fine!",
+  warning: "Oops, something went wrong.",
+  info: "You know what?",
+};
+
+export const Alert: React.FC<AlertProps> = ({
+  onClose,
+  setActive,
+  type,
+  text,
+  active,
+}) => {
   function handleClick() {
-    props.setActive(false);
-    if (props.onClose) {
-      props.onClose();
+    setActive(false);
+    if (onClose) {
+      onClose();
     }
   }
 
   return (
     <div
-      className={`custom-alert custom-alert-${props.type} ${
-        props.active ? "alert-active" : "alert-inactive"
+      className={`custom-alert custom-alert ${
+        active ? "alert-active" : "alert-inactive"
       }`}
     >
-      <p>{props.text}</p>
+      <h5>{TEXT_MAP[type]}</h5>
+      <p className="alert-text">{text}</p>
       <img
         src="/images/icons/close.svg"
         onClick={() => handleClick()}
