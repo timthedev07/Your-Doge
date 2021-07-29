00c076f308b5a17d4ae4a14d756f6562dd453f9a
Tim <harvard90873@gmail.com>
2021-07-29 22:26:54 +0200

    added url constant

--- client/src/constants/general.ts
+++ client/src/constants/general.ts
@@ -1,2 +1,7 @@
 export const unknownErrMsg =
   "Sorry, an unknown error occurred, try again later, or contact our support team(yourdoge.team@gmail.com)";
+
+export const FRONTEND_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://yourdoge.vercel.app"
+    : "http://localhost:3000";
