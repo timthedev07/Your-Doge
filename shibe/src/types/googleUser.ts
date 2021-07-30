ece9cae0e327fc7777ed432d448fbd1e28dfd060
Tim <harvard90873@gmail.com>
2021-07-30 17:57:53 +0200

    add decorators so it can be treated as an argument

--- shibe/src/types/googleUser.ts
+++ shibe/src/types/googleUser.ts
@@ -1,11 +1,31 @@
-export interface GoogleUser {
+import { Field, ArgsType } from "type-graphql";
+
+@ArgsType()
+export class GoogleUser {
+  @Field()
   id: string;
+
+  @Field()
   email: string;
+
+  @Field()
   verified_email: boolean;
+
+  @Field()
   name: string;
+
+  @Field()
   given_name: string;
+
+  @Field()
   family_name: string;
+
+  @Field()
   picture: string;
+
+  @Field()
   locale: string;
+
+  @Field()
   hd: string;
 }
