7309ffa3cc2e9e9ddb3dbca3efb1f2ccf43a7b9c
Tim <harvard90873@gmail.com>
2021-07-30 23:03:12 +0200

    fix: camelCase
    add: refreshToken function

--- client/src/lib/oauth/discord.ts
+++ client/src/lib/oauth/discord.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 const API_ENDPOINT = "https://discord.com/api/v8";
 const CLIENT_ID = process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID || "";
 
-export const exchange_code = async (code: string, clientSecret: string) => {
+export const exchangeCode = async (code: string, clientSecret: string) => {
   const { data } = await axios({
     url: `${API_ENDPOINT}/oauth2/token`,
     method: "post",
@@ -14,6 +14,27 @@ export const exchange_code = async (code: string, clientSecret: string) => {
       grant_type: "authorization_code",
       code: code,
       redirect_uri: FRONTEND_URL + "/auth/oauth/discord",
+    },
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+
+  return data;
+};
+
+export const refreshToken = async (
+  refreshToken: string,
+  clientSecret: string
+) => {
+  const { data } = await axios({
+    url: `${API_ENDPOINT}/oauth2/token`,
+    method: "post",
+    data: {
+      client_id: CLIENT_ID,
+      client_secret: clientSecret,
+      grant_type: "refresh_token",
+      refresh_token: refreshToken,
     },
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
