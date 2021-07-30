5ef6793c29227e28c3b31701fda22153353db372
Tim <harvard90873@gmail.com>
2021-07-30 13:49:42 +0200

    random slug generator

---
+++ shibe/src/constants/slugs.ts
@@ -0,0 +1,59 @@
+const adjective1 = [
+  "stinky",
+  "legitimate",
+  "brave",
+  "random",
+  "stupid",
+  "polluted",
+  "brain-washed",
+  "cheerful",
+  "warm",
+  "hair-rasing",
+];
+const adjective2 = [
+  "smelly",
+  "disrespectful",
+  "honorable",
+  "awesome",
+  "flaccid",
+  "wasted",
+  "smartass",
+];
+const noun = [
+  "salmon",
+  "kayboard",
+  "sunshine",
+  "drumstick",
+  "man",
+  "shepherd",
+  "bot",
+  "life",
+  "school",
+  "socks",
+  "facebook",
+  "CEO",
+  "guy",
+  "cake",
+  "dude",
+  "burrito",
+  "paella",
+  "pizza",
+  "dumplings",
+  "pepsi",
+  "meme",
+  "CaptainAmerica",
+  "Thor",
+  "franchise",
+  "city",
+];
+const words = { adjective1, adjective2, noun } as any;
+const order = ["adjective1", "adjective2", "noun"];
+
+export const randSlug = () => {
+  const selected = order.map((partOfSpeech) => {
+    const choices = words[partOfSpeech];
+    return choices[Math.floor(Math.random() * choices.length)];
+  });
+
+  return selected.join("-");
+};
