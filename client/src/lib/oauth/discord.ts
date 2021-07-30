b157492840b86b58c69ec9eb9117dadd797bb64c
Tim <harvard90873@gmail.com>
2021-07-30 22:46:22 +0200

    tweak some stuff

--- client/src/lib/oauth/discord.ts
+++ client/src/lib/oauth/discord.ts
@@ -2,19 +2,18 @@ import { FRONTEND_URL } from "../../constants/general";
 import axios from "axios";
 
 const API_ENDPOINT = "https://discord.com/api/v8";
-const CLIENT_ID = "870745774852567091";
-const CLIENT_SECRET = "937it3ow87i4ery69876wqire";
+const CLIENT_ID = process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID || "";
 
-export const exchange_code = async (code: string) => {
+export const exchange_code = async (code: string, clientSecret: string) => {
   const { data } = await axios({
     url: `${API_ENDPOINT}/oauth2/token`,
     method: "post",
     data: {
       client_id: CLIENT_ID,
-      client_secret: CLIENT_SECRET,
+      client_secret: clientSecret,
       grant_type: "authorization_code",
       code: code,
-      redirect_uri: FRONTEND_URL,
+      redirect_uri: FRONTEND_URL + "/auth/oauth/discord",
     },
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
