e3254761360be466f95c185ae0e005ae86fa475d
Tim <harvard90873@gmail.com>
2021-07-30 16:25:53 +0200

    create GoogleUser interface

---
+++ shibe/src/types/googleUser.ts
@@ -0,0 +1,11 @@
+export interface GoogleUser {
+  id: string;
+  email: string;
+  verified_email: boolean;
+  name: string;
+  given_name: string;
+  family_name: string;
+  picture: string;
+  locale: string;
+  hd: string;
+}
