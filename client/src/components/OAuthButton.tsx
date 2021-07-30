783508fee7b24bf0156276a00b39fca36820673b
Tim <harvard90873@gmail.com>
2021-07-30 20:40:46 +0200

    add classes and update button text

--- client/src/components/OAuthButton.tsx
+++ client/src/components/OAuthButton.tsx
@@ -3,22 +3,19 @@ import React from "react";
 
 interface OAuthButtonProps {
   provider: "google" | "twitter" | "facebook";
-  action: "Sign in" | "Sign up";
   href: string;
 }
 
-export const OAuthButton: React.FC<OAuthButtonProps> = ({
-  action,
-  provider,
-  href,
-}) => {
+export const OAuthButton: React.FC<OAuthButtonProps> = ({ provider, href }) => {
   return (
     <Link href={href} passHref={true}>
-      <button className="signin-button">
-        {`${action} with ${
-          provider.charAt(0).toUpperCase() + provider.slice(1)
-        }`}
-        <img src={`/images/icons/oauth/${provider}.svg`} alt="" />
+      <button className={`oauth-button oauth-button-${provider}`}>
+        {provider.charAt(0).toUpperCase() + provider.slice(1)}
+        <img
+          className="oauth-button__icon"
+          src={`/images/icons/oauth/${provider}.svg`}
+          alt=""
+        />
       </button>
     </Link>
   );
