01d09eb202a0e8b4984e3b72e6b2743cbdf883ec
Tim <harvard90873@gmail.com>
2021-07-30 23:03:34 +0200

    add constant discord authorization url

---
+++ client/src/constants/discord.ts
@@ -0,0 +1,2 @@
+export const discordOAuthUrl =
+  "https://discord.com/api/oauth2/authorize?client_id=870745774852567091&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Foauth%2Fdiscord&response_type=code&scope=identify%20email";
