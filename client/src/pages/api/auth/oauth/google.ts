beed85d974fde282196fedb1e4106541da3a1eca
Tim <harvard90873@gmail.com>
2021-07-30 22:33:59 +0200

    update authentication flow
    moved the `code` query string processing function to serverless api
    
    so that the secret would not get exposed, which is great

---
+++ client/src/pages/api/auth/oauth/google.ts
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getGoogleUserInfo } from "../../../../lib/oauth/google";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "POST") {
+    const { code } = req.body;
+
+    const result = await getGoogleUserInfo(code);
+
+    return res.json(result);
+  }
+
+  res.end("Method Not Allowed");
+};
+
+export default handler;
