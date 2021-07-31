0d40b26133898d80785a7decb97abcb0025fba90
Tim <harvard90873@gmail.com>
2021-07-31 16:14:27 +0200

    update discordUser.ts
    modify discord user structure and add decorators

--- shibe/src/types/discordUser.ts
+++ shibe/src/types/discordUser.ts
@@ -1,15 +1,31 @@
-export interface DiscordUser {
-  accent_color: any;
+import { Field, ArgsType } from "type-graphql";
+
+@ArgsType()
+export class DiscordUser {
+  @Field(() => String, { nullable: true })
+  accent_color: string | null | undefined;
+  @Field(() => String, { nullable: true })
   avatar: string | null | undefined;
-  banner: any;
-  banner_color: any;
+  @Field(() => String, { nullable: true })
+  banner: string | null | undefined;
+  @Field(() => String, { nullable: true })
+  banner_color: string | null | undefined;
+  @Field()
   discriminator: string;
+  @Field(() => String, { nullable: true })
   email: string | null | undefined;
+  @Field()
   flags: number;
+  @Field()
   id: string;
+  @Field()
   locale: string;
+  @Field()
   mfa_enabled: boolean;
+  @Field()
   public_flags: number;
+  @Field()
   username: string;
+  @Field()
   verified: boolean;
 }
