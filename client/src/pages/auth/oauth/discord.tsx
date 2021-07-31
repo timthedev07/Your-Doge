07140096387534d91deb09dfab0acc99fe88b632
Tim <harvard90873@gmail.com>
2021-07-31 21:42:49 +0200

    complete discord oauth client side page

--- client/src/pages/auth/oauth/discord.tsx
+++ client/src/pages/auth/oauth/discord.tsx
@@ -1,10 +1,18 @@
 import React, { useEffect } from "react";
 import * as queryString from "query-string";
 import axios from "axios";
+import {
+  MeDocument,
+  MeQuery,
+  useDiscordOAuthMutation,
+} from "../../../generated/graphql";
+import { useRouter } from "next/router";
 
 const Discord: React.FC = () => {
   const urlParams = queryString.parse(window.location.search);
   let code = urlParams.code as string;
+  const [registerDiscordUser] = useDiscordOAuthMutation();
+  const { push } = useRouter();
 
   useEffect(() => {
     const asyncFunc = async () => {
@@ -23,11 +31,28 @@ const Discord: React.FC = () => {
         },
       });
 
-      console.log(data);
+      const res = await registerDiscordUser({
+        variables: data,
+        update: (store, { data }) => {
+          if (!data) return null;
+          store.writeQuery<MeQuery>({
+            query: MeDocument,
+            data: {
+              __typename: "Query",
+              me: data.discordOAuth.user,
+            },
+          });
+          return null;
+        },
+      });
+
+      if (res.data?.discordOAuth.status === "logged-in") {
+        push("/dashboard");
+      }
     };
 
     asyncFunc();
-  }, [code]);
+  }, [code, registerDiscordUser, push]);
 
   return (
     <div className="email-confirmation">
