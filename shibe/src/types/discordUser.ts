f4c0c4e254c3fb0953d344008addc79a5ede0180
Tim <harvard90873@gmail.com>
2021-07-31 15:27:08 +0200

    add discord user type
    i'm not like entirely sure that this is the right structure

---
+++ shibe/src/types/discordUser.ts
@@ -0,0 +1,15 @@
+export interface DiscordUser {
+  accent_color: any;
+  avatar: string | null | undefined;
+  banner: any;
+  banner_color: any;
+  discriminator: string;
+  email: string | null | undefined;
+  flags: number;
+  id: string;
+  locale: string;
+  mfa_enabled: boolean;
+  public_flags: number;
+  username: string;
+  verified: boolean;
+}
