03969adfe2cf5d2c92e655a00bd89d9f2ac6bb9a
Tim <harvard90873@gmail.com>
2021-08-02 22:33:47 +0200

    error handling

--- client/src/pages/auth/oauth/google.tsx
+++ client/src/pages/auth/oauth/google.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import * as queryString from "query-string";
 import {
   MeDocument,
@@ -7,47 +7,64 @@ import {
 } from "../../../generated/graphql";
 import { useRouter } from "next/router";
 import axios from "axios";
+import { Alert } from "../../../components/Alert";
+import { unknownErrMsg } from "../../../constants/general";
 
 const Google: React.FC = () => {
   const urlParams = queryString.parse(window.location.search);
   let code = urlParams.code as string;
   const [registerGoogleUser] = useGoogleOAuthMutation();
   const { push } = useRouter();
+  const [active, setActive] = useState<boolean>(false);
+  const [alertMessage, setAlertMessage] = useState<string>("");
+
+  const displayError = (message: string) => {
+    setActive(true);
+    setAlertMessage(message);
+  };
 
   useEffect(() => {
     const asyncFunc = async () => {
       if (!code) {
         return;
       }
+      try {
+        const { data } = await axios({
+          url: "/api/auth/oauth/google",
+          method: "POST",
+          data: {
+            code,
+          },
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
 
-      const { data } = await axios({
-        url: "/api/auth/oauth/google",
-        method: "POST",
-        data: {
-          code,
-        },
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+        const res = await registerGoogleUser({
+          variables: { ...data },
+          update: (store, { data }) => {
+            if (!data) return null;
+            store.writeQuery<MeQuery>({
+              query: MeDocument,
+              data: {
+                __typename: "Query",
+                me: data.googleOAuth.user,
+              },
+            });
+            return null;
+          },
+        });
 
-      const res = await registerGoogleUser({
-        variables: { ...data },
-        update: (store, { data }) => {
-          if (!data) return null;
-          store.writeQuery<MeQuery>({
-            query: MeDocument,
-            data: {
-              __typename: "Query",
-              me: data.googleOAuth.user,
-            },
-          });
-          return null;
-        },
-      });
-
-      if (res.data?.googleOAuth.status === "logged-in") {
-        push("/dashboard");
+        if (res.data?.googleOAuth.status === "logged-in") {
+          push("/dashboard");
+        } else {
+          displayError(unknownErrMsg);
+        }
+      } catch (err: any) {
+        if (err?.graphQLErrors[0]?.message) {
+          displayError(err.graphQLErrors[0].message);
+        }
+        displayError(unknownErrMsg);
       }
     };
 
@@ -55,15 +72,23 @@ const Google: React.FC = () => {
   }, [code, registerGoogleUser, push]);
 
   return (
-    <div className="email-confirmation">
-      <div className="email-confirmation-card">
-        <div>
-          <h2>You are almost there...</h2>
-          <h4>Please wait while we sign you in/up...</h4>
-          <img src="/images/wait.svg" alt="" style={{ width: "200px" }} />
+    <>
+      <Alert
+        setActive={setActive}
+        active={active}
+        type="warning"
+        text={alertMessage}
+      />
+      <div className="email-confirmation">
+        <div className="email-confirmation-card">
+          <div>
+            <h2>You are almost there...</h2>
+            <h4>Please wait while we sign you in/up...</h4>
+            <img src="/images/wait.svg" alt="" style={{ width: "200px" }} />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
