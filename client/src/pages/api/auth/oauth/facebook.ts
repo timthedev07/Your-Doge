b0baf63b0c5561aa6cef1e8d38f3e67ddb14f019
Tim <harvard90873@gmail.com>
2021-08-03 14:03:20 +0200

    complete facebook serverless api(not tested yet)

--- client/src/pages/api/auth/oauth/facebook.ts
+++ client/src/pages/api/auth/oauth/facebook.ts
@@ -1,3 +1,36 @@
-const handler = () => {};
+import { NextApiRequest, NextApiResponse } from "next";
+import {
+  getAccessTokenFromCode,
+  getFacebookUserData,
+} from "../../../../lib/oauth/facebook";
+import { FacebookUserData } from "../../../../types/types";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "POST") {
+    const { code } = req.body;
+
+    if (!code) {
+      return res.status(400).end("Bad Request");
+    }
+
+    const accessToken = await getAccessTokenFromCode(code);
+
+    if (!accessToken) {
+      return res.status(400).end("Bad Request");
+    }
+
+    const userData: FacebookUserData | undefined = await getFacebookUserData(
+      accessToken || ""
+    );
+
+    if (!userData) {
+      return res.status(400).end("Bad Request");
+    }
+
+    res.status(200).json(userData);
+  }
+
+  res.status(405).end("Method Not Allowed");
+};
 
 export default handler;
