6e41c9096bd1690b366c4071a52a5ae4dd5c4d98
Tim <harvard90873@gmail.com>
2021-08-03 20:42:55 +0200

    remove redundant code

--- client/src/components/ReadOnlyProfile.tsx
+++ client/src/components/ReadOnlyProfile.tsx
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import AvatarData from "../../avatarData.json";
 import ReactMarkdown from "react-markdown";
 import { ReadOnlyProfileProps } from "../types/props";
-import { AvatarKeyType } from "../types/types";
 
 export const ReadOnlyProfile: React.FC<ReadOnlyProfileProps> = ({
   avatarId,
@@ -11,28 +10,14 @@ export const ReadOnlyProfile: React.FC<ReadOnlyProfileProps> = ({
   bio,
   age,
 }) => {
-  const [avatarSrc, setAvatarSrc] = useState<string>("");
-
-  const getSrcById = async (
-    id: AvatarKeyType,
-    setter: (arg: string) => any
-  ) => {
-    const { default: res } = await import(
-      `../../public/images/avatars/${AvatarData[id]}.svg` // use different avatars based on the avatarId
-      // binded to the user and stored in the database
-    );
-    setter(res.src as string);
-  };
-
-  /* this part gets the right avatar based on activeAvatar */
-  useEffect(() => {
-    getSrcById(avatarId, setAvatarSrc);
-  }, [avatarId]);
-
   return (
     <>
       <div className="profile-top-section">
-        <img alt="" className="avatar" src={avatarSrc} />
+        <img
+          alt=""
+          className="avatar"
+          src={`/images/avatars/${AvatarData[avatarId]}.svg`}
+        />
         <div className="profile-top-section__headings">
           <h2>{username}</h2>
           <h5>{email}</h5>
