a72bc195c85bc04c9aa8cfe420316e7f55d32e4e
Tim <harvard90873@gmail.com>
2021-08-03 13:47:45 +0200

    update facebook button url

--- client/src/components/OAuthButtons.tsx
+++ client/src/components/OAuthButtons.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { discordOAuthUrl } from "../lib/oauth/discord";
+import { facebookOAuthUrl } from "../lib/oauth/facebook";
 import { googleAuthUrl } from "../lib/oauth/google";
 import { OAuthButton } from "./OAuthButton";
 
@@ -7,7 +8,7 @@ export const OAuthButtons: React.FC = () => {
   return (
     <div className="oauth-buttons-wrapper">
       <OAuthButton provider="google" href={googleAuthUrl} />
-      <OAuthButton provider="facebook" href={googleAuthUrl} />
+      <OAuthButton provider="facebook" href={facebookOAuthUrl} />
       <OAuthButton provider="discord" href={discordOAuthUrl} />
     </div>
   );
