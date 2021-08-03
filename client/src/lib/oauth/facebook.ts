6f97959044f104e2396c2f2caf430cab87cbf691
Tim <harvard90873@gmail.com>
2021-08-03 14:03:03 +0200

    more utility functions

--- client/src/lib/oauth/facebook.ts
+++ client/src/lib/oauth/facebook.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import queryString from "query-string";
 import { FRONTEND_URL } from "../../constants/general";
 
@@ -9,5 +10,46 @@ const stringifiedParams = queryString.stringify({
   auth_type: "rerequest",
   display: "popup",
 });
+
+export const getAccessTokenFromCode = async (code: string) => {
+  try {
+    const { data } = await axios({
+      url: "https://graph.facebook.com/v4.0/oauth/access_token",
+      method: "get",
+      params: {
+        client_id: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID,
+        client_secret: process.env.FACEBOOK_CLIENT_SECRET,
+        redirect_uri: `${FRONTEND_URL}/auth/oauth/facebook`,
+        code,
+      },
+    });
+    /**
+     * data: { access_token: string, token_type: string, expires_in: number }
+     */
+    return data.access_token as string;
+  } catch (err) {
+    return "";
+  }
+};
+
+export const getFacebookUserData = async (accessToken: string) => {
+  try {
+    const { data } = await axios({
+      url: "https://graph.facebook.com/me",
+      method: "get",
+      params: {
+        fields: ["id", "email", "first_name", "last_name"].join(","),
+        access_token: accessToken,
+      },
+    });
+    console.log(data);
+    /**
+     * data: { id: idk?, email, first_name, last_name }
+     */
+    return data;
+  } catch (err) {
+    return undefined;
+  }
+};
 
 export const facebookOAuthUrl = `https://www.facebook.com/v4.0/dialog/oauth?${stringifiedParams}`;
