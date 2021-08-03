c613d99eee31a4e296c6b374a9aa9af85ed07e64
Tim <harvard90873@gmail.com>
2021-08-03 15:17:58 +0200

    wrap button with <a>

--- client/src/components/OAuthButton.tsx
+++ client/src/components/OAuthButton.tsx
@@ -8,15 +8,17 @@ interface OAuthButtonProps {
 
 export const OAuthButton: React.FC<OAuthButtonProps> = ({ provider, href }) => {
   return (
-    <Link href={href} passHref={true}>
-      <button className={`oauth-button oauth-button-${provider}`}>
-        {provider.charAt(0).toUpperCase() + provider.slice(1)}
-        <img
-          className="oauth-button__icon"
-          src={`/images/icons/oauth/${provider}.svg`}
-          alt=""
-        />
-      </button>
+    <Link href={href} passHref>
+      <a>
+        <button className={`oauth-button oauth-button-${provider}`}>
+          {provider.charAt(0).toUpperCase() + provider.slice(1)}
+          <img
+            className="oauth-button__icon"
+            src={`/images/icons/oauth/${provider}.svg`}
+            alt=""
+          />
+        </button>
+      </a>
     </Link>
   );
 };
