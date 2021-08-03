cbb987d11c42737ee2a552a3024eecb9babe8942
Tim <harvard90873@gmail.com>
2021-08-03 15:30:07 +0200

    initialize facebook oauth resolver(not tested yet)

--- shibe/src/resolvers/UserResolvers.ts
+++ shibe/src/resolvers/UserResolvers.ts
@@ -30,6 +30,7 @@ import { GoogleUser } from "../types/googleUser";
 import { DiscordUser } from "../types/discordUser";
 import { OAuthStatusType } from "../types/oauth";
 import { login, loginOAuth } from "../utils/login";
+import { FacebookUser } from "../types/facebookUser";
 
 const EMAIL_VALIDATION_REGEX =
   /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
@@ -474,7 +475,7 @@ export class UserResolver {
 
     if (user) {
       if (user.provider === "google") {
-        return loginOAuth(user, response, "logged-in");
+        return loginOAuth(user, response);
       } else {
         throw new Error("Email already linked with another account.");
       }
@@ -510,7 +511,7 @@ export class UserResolver {
       };
     }
 
-    return loginOAuth(user!, response, "logged-in");
+    return loginOAuth(user!, response);
   }
 
   @Mutation(() => OAuthResponse)
@@ -535,7 +536,7 @@ export class UserResolver {
 
     if (user) {
       if (user.provider === "discord") {
-        return loginOAuth(user, response, "logged-in");
+        return loginOAuth(user, response);
       } else {
         throw new Error("Email already linked with another account.");
       }
@@ -571,6 +572,50 @@ export class UserResolver {
       };
     }
 
-    return loginOAuth(user!, response, "logged-in");
+    return loginOAuth(user!, response);
+  }
+  @Mutation(() => OAuthResponse)
+  async facebookOAuth(
+    @Args(() => FacebookUser) userData: FacebookUser,
+    @Ctx() { res: response }: MyContext
+  ): Promise<OAuthResponse> {
+    await userCleanup();
+
+    const res = await registerUser();
+    if (res < 0) {
+      throw new Error("Sorry, registration is temporarily closed.");
+    }
+
+    const email = userData.email;
+
+    if (!email || !email.length || !validateEmailRegex(email)) {
+      throw new Error("Invalid email.");
+    }
+
+    let user: User | undefined = await User.findOne({ where: { email } });
+
+    if (user) {
+      if (user.provider === "facebook") {
+        return loginOAuth(user, response);
+      } else {
+        throw new Error("Email already linked with another account.");
+      }
+    }
+
+    try {
+      const insertRes = await User.insert({
+        email: email,
+        username: `${randSlug()}${userData.id.slice(userData.id.length - 4)}`,
+        serverId: res,
+        confirmed: true,
+        provider: "discord",
+      });
+
+      console.log(insertRes);
+    } catch (err) {}
+
+    user = await User.findOne({ where: { email } });
+
+    return loginOAuth(user!, response);
   }
 }
