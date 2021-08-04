df26286c62de9380d3f9e256f2ceee0ce3967d81
Tim <harvard90873@gmail.com>
2021-08-04 22:17:44 +0200

    check for time elapsed between the last update

--- shibe/src/resolvers/UserResolvers.ts
+++ shibe/src/resolvers/UserResolvers.ts
@@ -33,6 +33,7 @@ import { OAuthStatusType } from "../types/oauth";
 import { login, loginOAuth } from "../utils/login";
 import { FacebookUser } from "../types/facebookUser";
 import { isAuth } from "../utils/isAuthMiddleWare";
+import { daysElapsed } from "../utils/date";
 
 const EMAIL_VALIDATION_REGEX =
   /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
@@ -668,6 +669,12 @@ export class UserResolver {
       throw new Error("Wrong password");
     }
 
+    const days = daysElapsed(user.unameLastUpdate);
+    if (days < 60) {
+      throw new Error(
+        `You can't update your username until ${days} days after.`
+      );
+    }
     // all checks passed
 
     try {
