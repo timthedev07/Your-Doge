059d37343b9e7a49da3cb7a53ebd2734f683804d
Tim <harvard90873@gmail.com>
2021-08-06 16:46:05 +0200

    new subject entity

---
+++ shibe/src/entity/Subject.ts
@@ -0,0 +1,14 @@
+import { Field, Int, ObjectType } from "type-graphql";
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
+
+@ObjectType()
+@Entity("homework")
+export class Subject extends BaseEntity {
+  @Field(() => Int)
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field(() => String)
+  @Column({ type: "varchar", length: "100" })
+  name: string;
+}
