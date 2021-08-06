e7bb37a0e9cb6b070899827dcf558e4aefb935fe
Tim <harvard90873@gmail.com>
2021-08-06 16:45:51 +0200

    remove useless comment

--- burrito/src/entity/Homework.ts
+++ burrito/src/entity/Homework.ts
@@ -4,10 +4,6 @@ import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
 @ObjectType()
 @Entity("homework")
 export class Homework extends BaseEntity {
-  /**
-   * Here we are basically creating columns for our database
-   */
-
   @Field(() => Int)
   @PrimaryGeneratedColumn()
   id: number;
