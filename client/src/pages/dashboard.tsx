8ffa7022169d159f3a831e3119a1a1a84d5a8f7a
Tim <harvard90873@gmail.com>
2021-08-07 12:21:44 +0200

    update dashboard.tsx
    finally got the content loader working and responsive

--- client/src/pages/dashboard.tsx
+++ client/src/pages/dashboard.tsx
@@ -1,8 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import Calendar from "react-calendar";
 import { useApollo } from "../contexts/ApolloContext";
-import { useGetProfileQuery as useAllUserHomeworkQuery } from "../generated/graphql";
+import {
+  useGetProfileQuery as useAllUserHomeworkQuery,
+  useSubjectsQuery,
+} from "../generated/graphql";
 import { MarkRecordValue } from "../types/types";
+import ContentLoader from "react-content-loader";
 
 const Dashboard: React.FC = () => {
   // const [marks] = useState<Record<string, MarkRecordValue>>({});
@@ -45,9 +49,94 @@ const Dashboard: React.FC = () => {
   //   }
   // }, [gqlData, marks]);
 
+  const { data: subjectsData, loading: subjectsLoading } = useSubjectsQuery();
+
+  // a map that maps subject id to subject name
+  // undefined is when the query is still loading
+  const subjectsMap: Record<number, string> | undefined = useMemo(() => {
+    if (!subjectsData) {
+      return;
+    }
+
+    const res: Record<number, string> = {};
+
+    subjectsData?.subjects.forEach((each) => {
+      res[each.id] = each.name;
+    });
+
+    return res;
+  }, [subjectsData]);
+
+  const fakeHomework = [
+    {
+      id: 0,
+      title: "Homework 0",
+      description: "Dumb homework",
+      deadline: "",
+      subjectId: 3,
+      done: false,
+      onTime: false,
+      enjoyed: false,
+    },
+    {
+      id: 1,
+      title: "Homework 1",
+      description: "Still dumb homework",
+      deadline: "",
+      subjectId: 15,
+      done: true,
+      onTime: true,
+      enjoyed: false,
+    },
+  ];
+
   return (
     <div className="dashboard-container">
       <h1 className="dashboard-heading">Dashboard</h1>
+
+      <table className="homework-table">
+        {!subjectsLoading ? (
+          <div
+            style={{
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              justifyContent: "center",
+              paddingLeft: "10px",
+              paddingRight: "10px",
+              paddingTop: "20px",
+            }}
+          >
+            <ContentLoader
+              foregroundColor="#454545"
+              backgroundColor="#383838"
+              height={"280px"}
+              width={"100%"}
+            >
+              <rect x="10" y="15" rx="5" ry="5" width="98%" height="40" />
+              <rect x="10" y="75" rx="5" ry="5" width="98%" height="40" />
+              <rect x="10" y="135" rx="5" ry="5" width="98%" height="40" />
+              <rect x="10" y="195" rx="5" ry="5" width="98%" height="40" />
+            </ContentLoader>
+          </div>
+        ) : subjectsLoading ? (
+          ""
+        ) : (
+          <>
+            <tr>
+              <th>Title</th>
+              <th>Subject</th>
+            </tr>
+            {fakeHomework.map((each) => (
+              <tr key={each.title} className="homework-table-item">
+                <td>{each.title}</td>
+                <td>{subjectsMap![each.subjectId]}</td>
+              </tr>
+            ))}
+          </>
+        )}
+      </table>
+
       <Calendar
         className="big-ass-calendar"
         // tileClassName={({ date }) => {
