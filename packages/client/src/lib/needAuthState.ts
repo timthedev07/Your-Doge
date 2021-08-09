76e48361e845913e58a8db839e7ffb0854112207
Tim <harvard90873@gmail.com>
2021-08-09 21:44:18 +0200

    function docs

--- packages/client/src/lib/needAuthState.ts
+++ packages/client/src/lib/needAuthState.ts
@@ -1,3 +1,10 @@
+/**
+ * A helper function that decides if a route needs to access the current user data.
+ *
+ * This is useful when on pages like contact, or faq, we don't care about whether you are authenticated, so this can avoid the initial loading time and directly show user the content.
+ * @param path
+ * @returns
+ */
 export const needAuthState = (path: string): boolean => {
   const authStateRequiredRoutes: Record<string, boolean> = {
     "/dashboard": true,
