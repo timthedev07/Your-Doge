eac55c6905b40294149e7775d1052a1b90b4838c
Tim <harvard90873@gmail.com>
2021-08-12 16:37:17 +0200

    add class to tags

--- packages/client/src/pages/dashboard.tsx
+++ packages/client/src/pages/dashboard.tsx
@@ -165,7 +165,9 @@ const Dashboard: React.FC = () => {
                   <li key={each.id} className="homework-item">
                     <div className="homework-item-innermain">
                       <div className="homework-title">{each.title}</div>
-                      <div className="homework-tag">{each.tag}</div>
+                      <div className={`homework-tag ${each.tag}`}>
+                        {each.tag}
+                      </div>
                     </div>
                     <div className="homework-subject">
                       {subjectsMap![each.subjectId]}
