3ab85308b0357c555bcf486417630e1b292f3e4d
Tim <harvard90873@gmail.com>
2021-08-14 18:52:00 +0200

    add new helper function

--- packages/shared/src/utils/date.ts
+++ packages/shared/src/utils/date.ts
@@ -1,3 +1,7 @@
 export const daysElapsed = (dateInMilliseconds: number) => {
   return (new Date().valueOf() - dateInMilliseconds) / (1000 * 60 * 60 * 24);
 };
+
+export const daysToMilliseconds = (days: number) => {
+  return 1000 * 60 * 60 * 24 * days;
+};
