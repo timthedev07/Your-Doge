a8a2112864a9beb2b234566e9b4d27cf08667b10
Tim <harvard90873@gmail.com>
2021-08-15 21:34:54 +0200

    add subjects setup resolver for production

--- packages/shibe/src/resolvers/SubjectResolver.ts
+++ packages/shibe/src/resolvers/SubjectResolver.ts
@@ -4,6 +4,47 @@ import { Subject } from "../entity/Subject";
 @Resolver()
 export class SubjectResolver {
   @Mutation(() => Boolean)
+  async setupSubjects() {
+    const subjects: string[] = [
+      "Math",
+      "Physics",
+      "Chemistry",
+      "Biology",
+      "English",
+      "Spanish",
+      "Drama",
+      "Art",
+      "Dance",
+      "ICT",
+      "Geography",
+      "History",
+      "German",
+      "French",
+      "PE",
+      "Russian",
+      "Italian",
+      "Science",
+      "Social Studies",
+      "Valencian",
+      "Business Study",
+      "Computer Science",
+      "Economics",
+      "Chinese",
+      "Japanese",
+    ];
+
+    for (const subject of subjects) {
+      try {
+        await Subject.insert({ name: subject });
+      } catch (err) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  @Mutation(() => Boolean)
   async addSubject(@Arg("name") name: string) {
     if (!name) {
       return false;
