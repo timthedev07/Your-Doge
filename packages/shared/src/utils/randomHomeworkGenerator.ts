076a18dbe6b5df8a2dff39886699c52021c71f4d
Tim <harvard90873@gmail.com>
2021-08-16 18:00:47 +0200

    add random data generator

---
+++ packages/shared/src/utils/randomHomeworkGenerator.ts
@@ -0,0 +1,31 @@
+import { randSlug } from "./slugs";
+
+const temp = [
+  "urgent",
+  "hard",
+  "long-term",
+  "easy",
+  "normal",
+  "hard-and-urgent",
+];
+
+export const randomHomework = (n: number) => {
+  return Array.from(Array(n).keys()).map((each) => {
+    return {
+      __typename: "Homework",
+      id: each,
+      title: `${randSlug()}`,
+      description: "You hate dancin",
+      deadline: 1630620000000,
+      subjectId: Math.ceil(Math.random() * 20),
+      done: Math.random() < 0.5,
+      onTime: true,
+      enjoyed: false,
+      topicName: "GCSE eglish literature language techniques",
+      userId: 3,
+      tag: temp[
+        Math.random() < 0.15 ? 0 : Math.ceil(Math.random() * (temp.length - 1))
+      ],
+    };
+  });
+};
