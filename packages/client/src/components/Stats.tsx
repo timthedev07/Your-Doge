57d6c2e4c8dd2bbdc196559595a55564c6c014bd
Tim <harvard90873@gmail.com>
2021-08-18 17:00:22 +0200

    add container

--- packages/client/src/components/Stats.tsx
+++ packages/client/src/components/Stats.tsx
@@ -22,40 +22,42 @@ export const Stats: React.FC = () => {
   return (
     <div>
       <h2>Subject Breakdown</h2>
-      <Donut
-        data={{
-          labels: Object.keys(frequency),
-          datasets: [
-            {
-              label: "Subjects",
-              data: Object.values(frequency),
-              backgroundColor: [
-                "rgba(255, 99, 132, 0.2)",
-                "rgba(54, 162, 235, 0.2)",
-                "rgba(255, 206, 86, 0.2)",
-                "rgba(75, 192, 192, 0.2)",
-                "rgba(153, 102, 255, 0.2)",
-                "rgba(255, 159, 64, 0.2)",
-                "rgba(255, 0, 255, 0.2)",
-                "rgba(0, 255, 255, 0.2)",
-                "rgba(205, 87, 0, 0.2)",
-              ],
-              borderColor: [
-                "rgba(255, 99, 132, 1)",
-                "rgba(54, 162, 235, 1)",
-                "rgba(255, 206, 86, 1)",
-                "rgba(75, 192, 192, 1)",
-                "rgba(153, 102, 255, 1)",
-                "rgba(255, 159, 64, 1)",
-                "rgba(255, 0, 255, 1)",
-                "rgba(0, 255, 255, 1)",
-                "rgba(205, 87, 0, 1)",
-              ],
-              borderWidth: 1,
-            },
-          ],
-        }}
-      />
+      <div className="char-container" id="subjects-donut-container">
+        <Donut
+          data={{
+            labels: Object.keys(frequency),
+            datasets: [
+              {
+                label: "Subjects",
+                data: Object.values(frequency),
+                backgroundColor: [
+                  "rgba(255, 99, 132, 0.2)",
+                  "rgba(54, 162, 235, 0.2)",
+                  "rgba(255, 206, 86, 0.2)",
+                  "rgba(75, 192, 192, 0.2)",
+                  "rgba(153, 102, 255, 0.2)",
+                  "rgba(255, 159, 64, 0.2)",
+                  "rgba(255, 0, 255, 0.2)",
+                  "rgba(0, 255, 255, 0.2)",
+                  "rgba(205, 87, 0, 0.2)",
+                ],
+                borderColor: [
+                  "rgba(255, 99, 132, 1)",
+                  "rgba(54, 162, 235, 1)",
+                  "rgba(255, 206, 86, 1)",
+                  "rgba(75, 192, 192, 1)",
+                  "rgba(153, 102, 255, 1)",
+                  "rgba(255, 159, 64, 1)",
+                  "rgba(255, 0, 255, 1)",
+                  "rgba(0, 255, 255, 1)",
+                  "rgba(205, 87, 0, 1)",
+                ],
+                borderWidth: 1,
+              },
+            ],
+          }}
+        />
+      </div>
     </div>
   );
 };
