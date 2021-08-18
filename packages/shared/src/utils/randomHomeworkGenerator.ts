9e7cf18aecfb99b9f2a78e9354d0629407d6b50d
Tim <harvard90873@gmail.com>
2021-08-18 23:01:29 +0200

    randomly select date

--- packages/shared/src/utils/randomHomeworkGenerator.ts
+++ packages/shared/src/utils/randomHomeworkGenerator.ts
@@ -9,6 +9,12 @@ const temp = [
   "hard-and-urgent",
 ];
 
+const randomDate = (start: Date, end: Date) => {
+  return new Date(
+    start.getTime() + Math.random() * (end.getTime() - start.getTime())
+  );
+};
+
 export const randomHomework = (n: number) => {
   return Array.from(Array(n).keys()).map((each) => {
     return {
@@ -16,7 +22,11 @@ export const randomHomework = (n: number) => {
       id: each,
       title: `${randSlug()}`,
       description: "You hate dancin",
-      deadline: 1630620000000,
+      deadline: (() => {
+        var d = randomDate(new Date("2021-08-01"), new Date("2021-08-31"));
+        d.setHours(0), d.setMinutes(0), d.setSeconds(0), d.setMilliseconds(0);
+        return d.valueOf();
+      })(),
       subjectId: Math.ceil(Math.random() * 10),
       done: Math.random() < 0.5,
       onTime: true,
