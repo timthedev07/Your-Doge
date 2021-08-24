d29675b66e929f7ba8aa4c04ddb8f6779d773254
Tim <harvard90873@gmail.com>
2021-08-24 20:53:51 +0200

    desktop package setup

--- packages/desktop/electron.js
+++ packages/desktop/electron.js
@@ -1,6 +1,4 @@
-const path = require("path");
-
-const { app, BrowserWindow, webFrame } = require("electron");
+const { app, BrowserWindow } = require("electron");
 const isDev = require("electron-is-dev");
 
 function createWindow() {
