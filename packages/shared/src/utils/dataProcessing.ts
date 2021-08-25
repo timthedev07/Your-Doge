ec5bb32be89ee598cd750aadb8bd98bc8a9420de
Tim <harvard90873@gmail.com>
2021-08-25 17:23:12 +0200

    docstring

--- packages/shared/src/utils/dataProcessing.ts
+++ packages/shared/src/utils/dataProcessing.ts
@@ -1,3 +1,9 @@
+/**
+ * Group a list of objects by a given key.
+ * @param data A list of objects of type T
+ * @param key The key you want you data to be grouped by
+ * @returns A map containing the grouped data
+ */
 export const groupArray = <T extends {}>(data: T[], key: keyof T) => {
   const res: Map<T[keyof T], T[]> = new Map();
 
