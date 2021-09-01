9c3de7ff6e8ca91dc708b715b051e733a55f4a07
Tim <harvard90873@gmail.com>
2021-09-01 17:09:34 +0200

    more css

--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -8,13 +8,18 @@ const Home = () => {
         <title>Home | Your Doge</title>
       </Head>
       <div className="landing-page">
-        <header>
+        <header className="landing-header">
           <img
             className="landing-header-image"
-            src="/images/landing-header.png"
+            src="/images/landing-header2.png"
             alt=""
           />
+          <div className="landing-header-caption">
+            <h2>Your time is valuable,</h2>
+            <h4>and we are here to save it.</h4>
+          </div>
         </header>
+        <img src="/images/icons/down-arrow.svg" alt="" />
         <section></section>
         <section></section>
       </div>
